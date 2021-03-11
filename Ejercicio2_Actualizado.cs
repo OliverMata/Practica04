2778ee2d690683b1d18aa267d7cbb47d94925a67
OliverMata <78970451+OliverMata@users.noreply.github.com>
2021-03-10 23:32:25 -0400

    Add files via upload

---
+++ Ejercicio2_Actualizado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.Un programa que pida al usuario 5 números reales (pista: necesitarás un array de "float") y luego los muestre en el orden contrario al que se introdujeron
+            float[] aNumero = new float[5];
+            Console.WriteLine("Introduzca los numeros: ");
+            for (int i = 0; i < 5; i++)
+            {
+                aNumero[i] = float.Parse(Console.ReadLine());
+               
+            }
+            Array.Sort(aNumero);
+            Array.Reverse(aNumero);
+            Console.Clear();
+            
+            foreach (float i in aNumero)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            Console.ReadKey();   
+        }
+    }
+}
---
+++ Ejercicio4_Actualizado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Un programa que pida al usuario 10 números y luego calcule y muestre.
+            int[] aNumeros  = new int[10];
+            int m = aNumeros[0];
+            Console.WriteLine("Introduzca un numero: ");
+
+            for (int X = 0; X < 9; X++)
+            {
+                
+                aNumeros[X] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Clear();
+            for (int i = 0; i < 9; i++)
+            {
+                if (aNumeros[i] > m)
+                {
+                    m = aNumeros[i];
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("El numero mas grande es: " + m);
+            Console.WriteLine();
+        }
+    }
+}
